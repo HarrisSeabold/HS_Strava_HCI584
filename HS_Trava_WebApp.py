6796bf0c518ecba3f16bd30d580c7b19b334ac90
HarrisSeabold <89414864+HarrisSeabold@users.noreply.github.com>
2023-06-27 21:49:41 -0500

    Added demo data analysis function
    Added in progress demo data analysis function to bypass issues with Strava

--- HS_Trava_WebApp.py
+++ HS_Trava_WebApp.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
 import requests
 import urllib3
+import os
+import matplotlib.pyplot as plt
+import io
+import base64
+from werkzeug.utils import secure_filename
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
@@ -44,6 +47,43 @@ def process():
     print(my_dataset[0]["map"]["summary_polyline"])
 
     return 'Form submitted successfully!'
+
+
+@app.route('/demodata_process', methods=['POST'])
+
+def demodata_process():
+    uploaded_file = request.files['fileupload']
+    
+    if uploaded_file.filename != '':
+        # Save the uploaded file with a secure filename
+        filename = secure_filename(uploaded_file.filename)
+        uploaded_file.save(os.path.join('uploads', filename))
+
+        with open(uploaded_file.filename, 'r') as file:
+            file_content = file.read()
+            
+            # Perform your analysis on the file content
+            # You can process the text, extract information, perform calculations, etc.
+            
+            line_count = file_content.count('\n') + 1
+            
+            x = ['Line Count']
+            y = [line_count]
+            plt.bar(x, y)
+            
+            # Convert the plot to an image
+            image = io.BytesIO()
+            plt.savefig(image, format='png')
+            image.seek(0)
+            plot_url = base64.b64encode(image.getvalue()).decode()
+        
+        # Delete the uploaded file
+        os.remove(uploaded_file.filename)
+        
+        return render_template('demodata_results.html', plot_url=plot_url)
+    else:
+        return 'No file selected!'
+
 
 if __name__ == '__main__':
     app.run(port=5000)
