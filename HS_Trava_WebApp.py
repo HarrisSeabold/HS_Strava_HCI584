835bdff093c3a92cb9ec684101743edc16a807f5
HarrisSeabold <89414864+HarrisSeabold@users.noreply.github.com>
2023-07-06 22:26:00 -0500

    Fixed several issues
    Fixed API issues, added graph creation

--- HS_Trava_WebApp.py
+++ HS_Trava_WebApp.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request
 import requests
 import urllib3
-import os
 import matplotlib.pyplot as plt
-import io
-import base64
 from werkzeug.utils import secure_filename
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+id = 109422
+secret = '1a1a3857c3f11f0da887e4bc5064162ecf575246'
+refresh = '918b3db21b495d1aaddeb81147d444d8718a5c23'
 
 app = Flask(__name__)
 
@@ -15,21 +16,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-@app.route('/process', methods=['POST'])
-
-def process():
-    clientid = request.form.get('strava_id')
-    clientrefresh = request.form.get('strava_refresh')
-    clientsecret = request.form.get('strava_secret')
-
+@app.route('/make_chart', methods=['POST'])
+def make_chart():
+    
     auth_url = "https://www.strava.com/oauth/token"
     activites_url = "https://www.strava.com/api/v3/athlete/activities"
-
+    
     payload = {
-        'client_id': clientid,
-        'client_secret': clientsecret,
-        'refresh_token': clientrefresh,
+        'client_id': id,
+        'client_secret': secret,
+        'refresh_token': refresh,
         'grant_type': "refresh_token",
         'f': 'json'
     }
@@ -43,14 +39,38 @@ def process():
     param = {'per_page': 200, 'page': 1}
     my_dataset = requests.get(activites_url, headers=header, params=param).json()
 
-    print(my_dataset[0]["name"])
-    print(my_dataset[0]["map"]["summary_polyline"])
+    index1 = int(request.form.get('index1'))
+    activity1 = my_dataset[index1]
+    index2 = int(request.form.get('index2'))
+    activity2 = my_dataset[index2]
+    metric = request.form.get('metric')
 
-    return 'Form submitted successfully!'
+    name1 = activity1.get('name', 'N/A')
+    metric1 = activity1.get(metric, 'N/A')
+
+    name2 = activity2.get('name', 'N/A')
+    metric2 = activity2.get(metric, 'N/A')
 
 
-@app.route('/demodata_process', methods=['POST'])
+    metric_label = metric  # Replace with an appropriate label for your metric
 
+    # Create data for the bar chart
+    activities = [name1, name2]
+    metrics = [metric1, metric2]
+
+    # Plot the bar chart
+    plt.bar(activities, metrics)
+    plt.xlabel('Activity')
+    plt.ylabel(metric_label)
+    plt.title('Comparison of Metrics')
+    plt.show()
+
+    #return "Bar chart generated"
+
+    return f"Activity 1: {name1}, {metric}: {metric1} | Activity 2: {name2}, {metric}: {metric2}"
+
+
+"""@app.route('/demodata_process', methods=['POST'])
 def demodata_process():
     uploaded_file = request.files['fileupload']
     
@@ -83,7 +103,7 @@ def demodata_process():
         return render_template('demodata_results.html', plot_url=plot_url)
     else:
         return 'No file selected!'
-
+"""
 
 if __name__ == '__main__':
     app.run(port=5000)
